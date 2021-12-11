3931970765ebf0e7638be92720bf864322723c65
Stanislav Katyushin <justtrace17@gmail.com>
2021-12-11 17:37:20 +0300

    browser init outside of request handler

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import headers from "./headers.js";
 import { firefox } from "playwright";
 
 const app = express();
+const browser = await firefox.launch({ headless: true });
+const context = await browser.newContext();
 
 app
   .use((req, res, next) => res.set(headers) && next())
   .get("/login/", (req, res) => res.send("deadtrace"))
   .get("/test/", async (req, res) => {
     const { URL } = req.query;
-    const browser = await firefox.launch({ headless: true });
-    const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(URL);
     await page.click("button#bt");
     const value = await page.inputValue("input#inp");
-    await browser.close();
+    await page.close();
     res.send(value);
   })
   .all("/*", (r) => r.res.send("deadtrace"));
